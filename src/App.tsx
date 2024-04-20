623f2cf1bbaf8a283b7ffebb39abeee7909b5a61
HidekiOkumura <okumurahidekikangeki@gmail.com>
2024-04-21 01:23:17 +0900

    simplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+const styles = {
+  container: css`
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  overflow: visible;
+  `
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div css={styles.container}>
+        <h1>Hello React {[...Array(count).keys()].map(() => "!").join("")}</h1>
+        <div>
+        <button onClick={() => setCount(count + 1)}> Push me! </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
